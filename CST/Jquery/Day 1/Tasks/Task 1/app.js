43d413bfc8ca4809e82af86ece01ee8a835f62d8
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2022-12-21 16:54:41 +0200

    enhancment

--- CST/Jquery/Day 1/Tasks/Task 1/app.js
+++ CST/Jquery/Day 1/Tasks/Task 1/app.js
@@ -1,46 +1,33 @@
 var current = 'about', img = 0;
 var complain_data = [];
 
-onload = function (){
+function Switch(new_crnt) {
+  $(`#${current}_div`).css('display', 'none');
+  $(`#${new_crnt}_div`).css('display', 'block');
+  current = new_crnt;
+}
+
+onload = function () {
 
   $('#gallery_div').css('display', 'none');
   $('#services_div').css('display', 'none');
   $('#complain_div').css('display', 'none');
   $('#complain_out_div').css('display', 'none');
 
-  $('#about_btn').click (function (){
-    $(`#${current}_div`).css('display', 'none');
-    $('#about_div').css('display', 'block');
-    current = 'about';
-  })
+  $('#about_btn').click(Switch.bind(this, 'about'))
 
-  $('#gallery_btn').click (function (){
-    $(`#${current}_div`).css('display', 'none');
-    $('#gallery_div').css('display', 'block');
-    current = 'gallery';
-  })
+  $('#gallery_btn').click(Switch.bind(this, 'gallery'))
 
-  $('#services_btn').click (function (){
-    $(`#${current}_div`).css('display', 'none');
-    $('#services_div').css('display', 'block');
-    current = 'services';
-  })
+  $('#services_btn').click(Switch.bind(this, 'services'))
 
-  $('#complain_btn').click (function () {
-    $(`#${current}_div`).css('display', 'none');
-    $('#complain_div').css('display', 'block');
-    current = 'complain';
-  })
+  $('#complain_btn').click(Switch.bind(this, 'complain'))
 
-  $('#send').click (function () {
-    $(`#${current}_div`).css('display', 'none');
-    $('#complain_out_div').css('display', 'block');
-    current = 'complain_out';
+  $('#send').click(function () {
+    Switch('complain_out');
 
     complain_data = [];
-    $('#complain_div :input').each(function(i){
-      complain_data.push(this.value)
-    })
+
+    $('#complain_div :input').each(function () { complain_data.push(this.value) });
 
     $('#complain_preview').html(`Name is ${complain_data[0]}<br>
                                 Email is ${complain_data[1]}<br>
@@ -48,20 +35,16 @@ onload = function (){
                                 Complain is ${complain_data[3]}`);
   })
 
-  $('#back').click(function (){
-    $(`#${current}_div`).css('display', 'none');
-    $('#complain_div').css('display', 'block');
-    current = 'complain';
-  })
+  $('#back').click(Switch.bind(this, 'complain'))
 
-  $('#left').click(function(){
-    img = (img-1+5)%5;
+  $('#left').click(function () {
+    img = (img + 4) % 5;
     $('#main').attr('src', `Img/${img}.jpg`);
   })
 
-  $('#right').click(function(){
-    img = (img+1)%5;
-    $('#main').attr('src',`Img/${img}.jpg`);
+  $('#right').click(function () {
+    img = (img + 6) % 5;
+    $('#main').attr('src', `Img/${img}.jpg`);
   })
 
 }
