50726ab2c329347a5c3a5deb8ba451f608646ba3
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-01-26 00:03:38 +0200

    bug fixes

--- C#/Day 2/Tasks/Task 4/Program.cs
+++ C#/Day 2/Tasks/Task 4/Program.cs
@@ -26,7 +26,7 @@ namespace Task_4
             #region Without trace
 
             sw = Stopwatch.StartNew();
-            val = PresentList1(183.23, 64.11, 7, 12, new double[] { 4.53, 9.11, 4.53, 6.00, 1.04, 0.87, 2.57, 19.45, 65.59, 14.14, 16.66, 13.53 },
+            val = PresentList2(183.23, 64.11, 7, 12, new double[] { 4.53, 9.11, 4.53, 6.00, 1.04, 0.87, 2.57, 19.45, 65.59, 14.14, 16.66, 13.53 },
                                               new double[] { 12.23, 45.03, 12.23, 32.93, 6.99, 0.46, 7.34, 65.98, 152.13, 7.23, 10.00, 25.25 });
 
             sw.Stop();
@@ -42,22 +42,10 @@ namespace Task_4
 
         public static double PresentList1(double budget, double bagVolume, int people, int Npresents, double[] presentVolume, double[] presentPrice)
         {
+            int[] arr = new int[Npresents];
+            Tuple<int[], double, double> list = Recurse(0, ref arr, 0, 0, 0);
 
-            Tuple<int[], double, double>[] list = new Tuple<int[], double, double>[Npresents-1];
-            double max = 0;
-
-            for (int i = 0; i < Npresents-1; i++)
-            {
-                int[] arr = new int[Npresents];
-                arr[0] = i;
-                list[i] = Recurse(i+1, ref arr, 1,presentVolume[i], presentPrice[i]);
-                max = list[i].Item3 > max? list[i].Item3 : max;
-            }
-
-            //var max = list.Max((l) => l.Item3);
-            //var index = list.Where((l) => l.Item3 == max).SingleOrDefault();
-            return max;
-
+            return list.Item3;
 
             Tuple <int[],double,double> Recurse(int i, ref int[] taken, int j,double vol, double spent)
             {
@@ -65,19 +53,23 @@ namespace Task_4
 
                 int[] tmp1 = (int[])taken.Clone();
                 int[] tmp2 = (int[])taken.Clone();
-                
+                int left = Npresents - i;
                 tmp1[j] = i;
-                
+
                 Tuple<int[], double, double> with = null, without = null;
 
-                if(taken.Length + Npresents - i - 1 >= people)
+                if (left - 1 >= people - (j % people))
                     without = Recurse(i + 1, ref tmp2, j, vol, spent);
 
-                if (vol + presentVolume[i] <= bagVolume && spent + presentPrice[i] <= budget)
-                    with = Recurse(i + 1, ref tmp1, j+1, vol + presentVolume[i], spent + presentPrice[i]);
+                if (((j + 1) % people == 0
+                    || left - 1 >= people - ((j + 1) % people))
+                    && vol + presentVolume[i] <= bagVolume
+                    && spent + presentPrice[i] <= budget)
+                    
+                    with = Recurse(i + 1, ref tmp1, j + 1, vol + presentVolume[i], spent + presentPrice[i]);
 
 
-                if(without == null && with == null) return new Tuple<int[], double, double> ( taken, vol, spent);
+                if (without == null && with == null) return new Tuple<int[], double, double>(taken, vol, spent);
                 else if (with == null && without != null) return without;
                 else if (with != null && without == null) return with;
                 else return with.Item3 > without.Item3 ? with : without;
@@ -88,36 +80,34 @@ namespace Task_4
 
         public static double PresentList2(double budget, double bagVolume, int people, int Npresents, double[] presentVolume, double[] presentPrice)
         {
-
-            double max = 0, tmp;
-
-            for (int i = 0; i < Npresents - 1; i++)
-            {
-                tmp = Recurse(i + 1, 1,presentVolume[i], presentPrice[i]);
-                max = tmp > max ? tmp : max;
-            }
-
-            return max;
+            return Recurse(0, 0, 0, 0);
 
 
-            double Recurse(int i, int taken,double vol, double spent)
-            {
-                if (i >= Npresents) return spent;
+             double Recurse(int i, int taken,double vol, double spent)
+             {
+                if (i >= Npresents)
+                {
+                    if (taken % people == 0) return spent;
+                    else return 0;
+                }
+                
+                int left = Npresents - i;
+
+                if(left < people - (taken % people)) 
+                    return spent;
 
                 double with = 0, without = 0;
 
-                if (taken + Npresents - i - 1 >= people)
+                if (left - 1 >= people - (taken % people))
                     without = Recurse(i + 1, taken, vol, spent);
 
-                if (vol + presentVolume[i] <= bagVolume && spent + presentPrice[i] <= budget)
-                    with = Recurse(i + 1, taken, vol + presentVolume[i], spent + presentPrice[i]);
+                if (((taken + 1) % people == 0 || left - 1 >= people - ((taken + 1) % people))
+                    && vol + presentVolume[i] <= bagVolume 
+                    && spent + presentPrice[i] <= budget)
+                    with = Recurse(i + 1, taken + 1, vol + presentVolume[i], spent + presentPrice[i]);
 
 
-                if (without == 0 && with == 0) return spent;
-                else if (with == 0 && without != 0) return without;
-                else if (with != 0 && without == 0) return with;
-                else return with > without? with : without;
-
+                return Math.Max(with, Math.Max(without, spent));
             }
         }
     }
