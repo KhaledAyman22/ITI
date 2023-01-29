1742cd08cc8396353d07fbcd8c822649268ef3fe
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-01-29 20:15:26 +0200

    replaced arrays with list

--- C#/Day 2/Tasks/Task 4/Program.cs
+++ C#/Day 2/Tasks/Task 4/Program.cs
@@ -7,73 +7,66 @@ namespace Task_4
         static void Main(string[] args)
         {
             Stopwatch sw;
-            TimeSpan ts;
-            double val;
 
             #region With Trace
             sw = Stopwatch.StartNew();
-            val = PresentList1(183.23, 64.11, 7, 12, new double[] { 4.53, 9.11, 4.53, 6.00, 1.04, 0.87, 2.57, 19.45, 65.59, 14.14, 16.66, 13.53 },
+            var val1 = PresentList1(183.23, 64.11, 7, 12, new double[] { 4.53, 9.11, 4.53, 6.00, 1.04, 0.87, 2.57, 19.45, 65.59, 14.14, 16.66, 13.53 },
                                               new double[] { 12.23, 45.03, 12.23, 32.93, 6.99, 0.46, 7.34, 65.98, 152.13, 7.23, 10.00, 25.25 });
+
+            string str = string.Join(",", val1.Item1.ToList());
 
             sw.Stop();
 
-            Console.WriteLine("Spent: " + val + "\nElapsed: " + sw.ElapsedMilliseconds + "ms");
+            Console.WriteLine($"Taken: {str}\nTaken Volume: {val1.Item2}/64.11\nSpent: {val1.Item3}/183.23\nElapsed: {sw.ElapsedMilliseconds}ms");
 
             #endregion
             
-            Console.WriteLine("\n\n\n\n\n");
+            Console.WriteLine("\n");
 
             #region Without trace
 
             sw = Stopwatch.StartNew();
-            val = PresentList2(183.23, 64.11, 7, 12, new double[] { 4.53, 9.11, 4.53, 6.00, 1.04, 0.87, 2.57, 19.45, 65.59, 14.14, 16.66, 13.53 },
+            var val2 = PresentList2(183.23, 64.11, 7, 12, new double[] { 4.53, 9.11, 4.53, 6.00, 1.04, 0.87, 2.57, 19.45, 65.59, 14.14, 16.66, 13.53 },
                                               new double[] { 12.23, 45.03, 12.23, 32.93, 6.99, 0.46, 7.34, 65.98, 152.13, 7.23, 10.00, 25.25 });
 
             sw.Stop();
 
-            ts = sw.Elapsed;
-            
-            Console.WriteLine("Spent: " + val + "\nElapsed: " + sw.ElapsedMilliseconds + "ms");
+            Console.WriteLine($"Spent: {val2}/183.23\nElapsed: {sw.ElapsedMilliseconds}ms");
 
             #endregion
-
-            Console.Read();
         }
 
-        public static double PresentList1(double budget, double bagVolume, int people, int Npresents, double[] presentVolume, double[] presentPrice)
+        public static Tuple<List<int>, double, double> PresentList1(double budget, double bagVolume, int people, int Npresents, double[] presentVolume, double[] presentPrice)
         {
-            int[] arr = new int[Npresents];
-            Tuple<int[], double, double> list = Recurse(0, ref arr, 0, 0, 0);
+            return Recurse(0, new List<int>(), 0, 0, 0); ;
 
-            return list.Item3;
 
-            Tuple <int[],double,double> Recurse(int i, ref int[] taken, int j,double vol, double spent)
+            Tuple <List<int>, double,double> Recurse(int i, List<int> taken, int j,double vol, double spent)
             {
-                if (i >= Npresents) return new Tuple<int[], double, double>(taken, vol, spent);
+                if (i >= Npresents) return new Tuple<List<int>, double, double>(taken, vol, spent);
 
-                int[] tmp1 = (int[])taken.Clone();
-                int[] tmp2 = (int[])taken.Clone();
+                List<int> tmp1 = new(taken);
+                List<int> tmp2 = new(taken);
                 int left = Npresents - i;
-                tmp1[j] = i;
+                tmp1.Add(i);
 
-                Tuple<int[], double, double> with = null, without = null;
+                Tuple<List<int>, double, double> with = null, without = null;
 
                 if (left - 1 >= people - (j % people))
-                    without = Recurse(i + 1, ref tmp2, j, vol, spent);
+                    without = Recurse(i + 1, tmp2, j, vol, spent);
 
-                if (((j + 1) % people == 0
-                    || left - 1 >= people - ((j + 1) % people))
+                if (
+                    ((j + 1) % people == 0 || left - 1 >= people - ((j + 1) % people))
                     && vol + presentVolume[i] <= bagVolume
-                    && spent + presentPrice[i] <= budget)
+                    && spent + presentPrice[i] <= budget
+                   )
                     
-                    with = Recurse(i + 1, ref tmp1, j + 1, vol + presentVolume[i], spent + presentPrice[i]);
+                    with = Recurse(i + 1, tmp1, j + 1, vol + presentVolume[i], spent + presentPrice[i]);
 
+                double w = with?.Item3 ?? 0, wo = without?.Item3 ?? 0;
 
-                if (without == null && with == null) return new Tuple<int[], double, double>(taken, vol, spent);
-                else if (with == null && without != null) return without;
-                else if (with != null && without == null) return with;
-                else return with.Item3 > without.Item3 ? with : without;
-
+                if(w == wo) return new Tuple<List<int>, double, double>(taken, vol, spent);
+                return w > wo ? with : without;
             }
         }
 
@@ -101,9 +94,11 @@ namespace Task_4
                 if (left - 1 >= people - (taken % people))
                     without = Recurse(i + 1, taken, vol, spent);
 
-                if (((taken + 1) % people == 0 || left - 1 >= people - ((taken + 1) % people))
+                if (
+                    ((taken + 1) % people == 0 || left - 1 >= people - ((taken + 1) % people))
                     && vol + presentVolume[i] <= bagVolume 
-                    && spent + presentPrice[i] <= budget)
+                    && spent + presentPrice[i] <= budget
+                   )
                     with = Recurse(i + 1, taken + 1, vol + presentVolume[i], spent + presentPrice[i]);
 
 
