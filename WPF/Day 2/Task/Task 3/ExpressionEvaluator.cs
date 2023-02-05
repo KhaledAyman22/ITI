0a42cd988870b75c87b18427db68e25b865d7405
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-02-05 15:34:32 +0200

    WPF Day 2

---
+++ WPF/Day 2/Task/Task 3/ExpressionEvaluator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    static class ExpressionEvaluator
+    {
+        public static double EvaluateExpression(string expression)
+        {
+            string currentNumber = "";
+            bool decimalPointEncountered = false;
+            Stack<double> values = new();
+            Stack<char> operators = new();
+
+            foreach (char c in expression)
+            {
+                if (char.IsDigit(c) || c == '.')
+                {
+                    currentNumber += c;
+                    if (c == '.')
+                    {
+                        decimalPointEncountered = true;
+                    }
+                }
+                else
+                {
+                    if (decimalPointEncountered)
+                    {
+                        values.Push(double.Parse(currentNumber));
+                    }
+                    else
+                    {
+                        values.Push(int.Parse(currentNumber));
+                    }
+                    currentNumber = "";
+                    decimalPointEncountered = false;
+
+                    while (operators.Count > 0 && HasHigherPrecedence(operators.Peek(), c))
+                    {
+                        ApplyOperator(operators, values);
+                    }
+                    operators.Push(c);
+                }
+            }
+
+            if (decimalPointEncountered)
+            {
+                values.Push(double.Parse(currentNumber));
+            }
+            else
+            {
+                values.Push(int.Parse(currentNumber));
+            }
+            //currentNumber = "";
+            //decimalPointEncountered = false;
+
+            while (operators.Count > 0)
+            {
+                ApplyOperator(operators, values);
+            }
+
+            return values.Pop();
+        }
+
+        public static bool HasHigherPrecedence(char operator1, char operator2)
+        {
+            if ((operator1 == '×' || operator1 == '÷') && (operator2 == '+' || operator2 == '-'))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void ApplyOperator(Stack<char> operators, Stack<double> values)
+        {
+            double value2 = values.Pop();
+            double value1 = values.Pop();
+            char op = operators.Pop();
+
+            switch (op)
+            {
+                case '+':
+                    values.Push(value1 + value2);
+                    break;
+
+                case '-':
+                    values.Push(value1 - value2);
+                    break;
+
+                case '×':
+                    values.Push(value1 * value2);
+                    break;
+
+                case '÷':
+                    values.Push(value1 / value2);
+                    break;
+            }
+        }
+
+    }
+}
---
+++ WPF/Day 2/Task/Task 3/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private char _divide = '÷';
+        private char _times = '×';
+        private char _minus = '-';
+        private char _plus = '+';
+        private char _equal = '=';
+        private char _dot = '.';
+
+        private string _str_operation = "";
+
+        private bool _startnew = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button num)
+            {
+                if (_startnew)
+                {
+                    _str_operation = "";
+                    _startnew = false;
+                }
+                _str_operation += GetNumber(num).ToString();
+                screen.Text = _str_operation;
+            }
+
+        }
+
+        private void OpClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button op)
+            {
+                char c = GetOperator(op);
+                
+                if (_startnew)
+                {
+                    if (c == _minus || c == _dot) {
+                        _startnew = false;
+                        _str_operation = "";
+                    }
+                    else return;
+                }
+
+                if (c != _equal) {
+                    _str_operation += c.ToString();
+                    screen.Text = _str_operation;
+                }
+
+                else
+                {
+                    _str_operation = ExpressionEvaluator.EvaluateExpression(_str_operation).ToString();
+                    screen.Text = _str_operation;
+                    _startnew = true;
+                }
+
+            }
+        }
+
+        private int GetNumber(Button num)
+        {
+            int tmp = num.Name.ToString() switch
+            {
+                "zero" => 0,
+                "one" => 1,
+                "two" => 2,
+                "three" => 3,
+                "four" => 4,
+                "five" => 5,
+                "six" => 6,
+                "seven" => 7,
+                "eight" => 8,
+                "nine" => 9,
+                _ => 0
+            };
+
+            return tmp;
+        }
+
+        private char GetOperator(Button op)
+        {
+            char tmp = op.Name.ToString() switch
+            {
+                "plus" => _plus,
+                "minus" => _minus,
+                "divide" => _divide,
+                "times" => _times,
+                "equal" => _equal,
+                "dot" => _dot,
+                _ => _plus
+            };
+
+            return tmp;
+        }
+
+
+    }
+}
+
