43c92639488727b86ad1ac9d06adbb230c75486b
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-02 14:34:31 +0200

    MVC Day 1

---
+++ MVC/Day 1/Tasks/Tasks/Task4/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tasks.Task4
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
