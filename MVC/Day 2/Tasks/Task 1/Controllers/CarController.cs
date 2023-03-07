5064ae8b4d3e5c187063c6feb106b07c3d4b1dea
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-07 15:29:45 +0200

    MVC Day 2

---
+++ MVC/Day 2/Tasks/Task 1/Controllers/CarController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task_1.Models;
+
+namespace Task_1.Controllers
+{
+    public class CarController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("AllCars");
+        }
+
+        public ActionResult AllCars()
+        {
+            return View(CarList.Cars);
+        }
+
+        
+        
+        public ActionResult CarById(int id)
+        {
+            var car = CarList.Cars.Where(c => c.Num == id).FirstOrDefault();
+            return View(car);
+        }
+
+
+
+
+        public ActionResult NewCar(Car car)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCar(Car car)
+        {
+            CarList.Cars.Add(car);
+
+            return RedirectToAction("AllCars");
+        }
+
+
+
+
+        public ActionResult UpdateCar(int id)
+        {
+            var car = CarList.Cars.Where(c => c.Num == id).FirstOrDefault();
+            return View(car);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCar(int id, string manufacture, string color, string model)
+        {
+            var car = CarList.Cars.Where(c => c.Num == id).FirstOrDefault();
+            car.Model = model;
+            car.Manufacture = manufacture;
+            car.Color = color;
+            return RedirectToAction("AllCars");
+        }
+
+        
+        
+        public ActionResult DeleteCar(int id)
+        {
+            var car = CarList.Cars.Where(c => c.Num == id).FirstOrDefault();
+            return View(car);
+        }
+
+        [HttpPost]
+        public ActionResult PerformDelete(int id)
+        {
+            var car = CarList.Cars.Where(c => c.Num == id).FirstOrDefault();
+            CarList.Cars.Remove(car);
+            return RedirectToAction("AllCars");
+        }
+    }
+}
---
+++ MVC/Day 2/Tasks/Task 2/Models/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task_2.Models
+{
+    public class Data
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+    }
+}
