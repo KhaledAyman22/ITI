c473ea66d7c8b58d00b72e8b0f33ea6518c1038b
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-22 14:19:56 +0200

    fixed update button disabled

--- C#/Day 15/HotelManagmentSystem/UI/MVVM/View/FrontendWindow.xaml.cs
+++ C#/Day 15/HotelManagmentSystem/UI/MVVM/View/FrontendWindow.xaml.cs
@@ -96,6 +96,8 @@ namespace UI.MVVM.View
             if(finalizePaymentWindow.ShowDialog() == true)
             {
                 ((FrontendViewModel)DataContext).SetPaymentData(finalizePaymentWindow.Data);
+
+                updateBtn.IsEnabled = true;
             }
         }
 
